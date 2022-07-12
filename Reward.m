489a38500977f407fab55c79456defa3aa448e4b
Karan Jagdale <37465796+KaranJagdale@users.noreply.github.com>
2022-07-12 14:50:13 -0500

    Most recent reward

--- Reward.m
+++ Reward.m
@@ -19,15 +19,24 @@
 %             pbs = min(arr_par(state(1)+1)*hw + lpass,acap);
 %         case 3
 %             pbs = min(arr_par(state(1)+1)*hw + lpass,acap);
-%     end    
+%     end
+    n_b = size(state,2);
+    ipl = iplus(bus,n_b); %bus behind the current bus
+    stateb = state(:,ipl); %State is state of all the modules
+    state = state(:,bus);
     n_s = 8;
     if state(3) == 1
         cap_bus = cap_bus*2;
     end
+    ippl = iplus(ipl,n_b);
+    pphead = hw(ippl);
     hw = hw(bus);
-    hwt = 200; %target headway
+    hwt = 120; %target headway
+    hwg = 90; %headway gain
     pbs = min(arr_par(state(1))*hw + lpass,cap_bus-state(2)); %this is approximation
-    %nxt_stp is the next stop to the stop to which the bus is approaching
+    %nxt_stp is the next stop to the stop to which the bus is approaching.
+    %lpass is 0
+   
     if state(1) == n_s
         nxt_stp = 1;
     else 
@@ -56,28 +65,63 @@
 %             pds = a_par(bus)*state(2);
 %     end
     if lsact == 1
-        pds = a_par(bus)*(state(2)-lapass) + lapass;
+        pds = a_par(state(1))*(state(2)-lapass) + lapass;
     else
-        pds = a_par(bus)*state(2);
+        pds = a_par(state(1))*state(2);
     end
 
     pbsplus = min(arr_par(nxt_stp)*hw + lpass,cap_bus - (state(2)+pbs - pds));  %assuming deboarding people = boarding people
     pdsplus = a_par(nxt_stp)*(state(2)-pds + pbs); 
   
-    fixJr = 1500; %this is fixed positive reward for join action since the logical computation is not possible
+    %fixJr = 1500; %this is fixed positive reward for join action since the logical computation is not possible
     %here the fix positive reward is used as joining the bus allows it to
     %split on some next station
     wst = dis_stp(nxt_stp)/v_pas;
     wstp  = dis_stp(nnxt_stp)/v_pas;
-    rskip = -pds*wst - pbs*phead + (state(2)-pds)*(pds*t_al + pbs*t_bo) + 10*(hw - hwt);% + pbsplus*(pds*t_al + pbs*t_bo);
+    rskip = -pds*wst - pbs*phead + (state(2)-pds)*(pds*t_al + pbs*t_bo) + hwg*(hw - hwt);% + pbsplus*(pds*t_al + pbs*t_bo);
     rstop = -rskip;
-    rsplit = (state(2)-pds)*(pds*t_al + pbs*t_bo) +pds*wst +pbs*phead + 10*(hw - hwt); % pbsplus*(pds*t_al + pbs*t_bo);
-    %disp(pbsplus)
-    rskipp = -pdsplus*wstp - pbsplus*phead + (state(2)-pdsplus)*(pdsplus*t_al + pbsplus*t_bo) + 10*(hw - hwt);
-    rsplitp = (state(2)-pdsplus)*(pdsplus*t_al + pbsplus*t_bo) +pdsplus*wst +pbsplus*phead + 10*(hw - hwt);
-    R1 = rsplitp;
-    R2 = abs(rskipp);
-    rjoin = R1- R2  - phead*state(2);
+    rsplit = (state(2)-pds)*(pds*t_al + pbs*t_bo) +pds*wst +pbs*phead + hwg*(hw - hwt); % pbsplus*(pds*t_al + pbs*t_bo);
+    %Now the reward for join
+%     fprintf('(state(2)-pds)*(pds*t_al + pbs*t_bo) = %f \n',(state(2)-pds)*(pds*t_al + pbs*t_bo))
+%     fprintf('pds*wst = %f \n',pds*wst)
+%     fprintf('pbs*phead = %f \n',pbs*phead)
+%     fprintf('10*(hw - hwt) = %f \n', 10*(hw - hwt))
+        
+    if action == 3        
+        stbkbus = [];
+        if stateb(1) < state(1) || stateb(1) == state(1)
+            for i=stateb(1):state(1)
+                stbkbus = [stbkbus i];
+            end
+        else
+            stbkbus = 1:size(a_par,2);
+            for i=state(1):stateb(1)
+                stbkbus(stbkbus == i) = [];
+            end
+        end
+        rjskipc = 0;
+        
+        for i=stbkbus
+            p_ds = a_par(i)*stateb(2);
+            l_i= stateb(2) - p_ds;  %intermediate load
+            p_bs = min(phead*arr_par(i),cap_bus - l_i);
+            %stateb(2) = stateb(2) + p_bs;
+            nxt_stp = iplus(i,size(a_par,2));
+            wst = dis_stp(nxt_stp)/v_pas;
+            rjskip = -p_ds*wst - p_bs*pphead + (stateb(2)-p_ds)*(p_ds*t_al + p_bs*t_bo);% + 10*(phead - hwt);
+            rjskipc = rjskipc + rjskip;
+            %disp(rjskip)
+        end
+        %disp(rjskipc)
+        %above is the cumulative skip reward as the back module has to skip some stops to catch up with the front module 
+
+
+        rskipp = -pdsplus*wstp - pbsplus*phead + (state(2)-pdsplus)*(pdsplus*t_al + pbsplus*t_bo) + hwg*(hw - hwt);
+        rsplitp = (state(2)-pdsplus)*(pdsplus*t_al + pbsplus*t_bo) +pdsplus*wst +pbsplus*phead + hwg*(hw - hwt);
+        R1 = rsplitp;
+        R2 = abs(rskipp);
+        rjoin = R1- R2  - phead*state(2) + rjskipc;
+    end
     
     switch action
         case 0
@@ -89,6 +133,7 @@
         case 3
             reward = rjoin;
     end
+ end
 %Reward for action 3(join) is probably impossible to compute as it depends
 %on the future load which depends on the current action which depends on
 %the current reward computation. So, writting the reward for action 3(join)
